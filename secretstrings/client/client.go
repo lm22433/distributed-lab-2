ebe3f43748e3e40abf5ebe54b3d4251d0e892c7b
Harry Greentree <harrygreentree@icloud.com>
2023-10-13 13:36:39 +0100

    Question 2 - Using RPC - Secret Strings (Stage 3)

--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	//	"net/rpc"
 	"flag"
 	"net/rpc"
+	"os"
 	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 
 	//	"bufio"
@@ -12,6 +14,21 @@ import (
 	"fmt"
 )
 
+func words() ([]string, error) {
+	f, err := os.Open("../wordlist")
+	if err != nil {
+		fmt.Println("Could not open wordlist file.")
+	}
+	defer f.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
 func main() {
 	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
 	flag.Parse()
@@ -19,9 +36,19 @@ func main() {
 	client, _ := rpc.Dial("tcp", *server)
 	defer client.Close()
 
-	request := stubs.Request{Message: "Hello"}
-	response := new(stubs.Response)
-	client.Call(stubs.PremiumReverseHandler, request, response)
-	fmt.Println("Responded:", response.Message)
-	//TODO: connect to the RPC server and send the request(s)
+	words, err := words()
+	if err != nil {
+		fmt.Println("Could not read words:", err)
+	}
+
+	for _, word := range words {
+		request := stubs.Request{Message: word}
+		response := new(stubs.Response)
+		err = client.Call(stubs.PremiumReverseHandler, request, response)
+		if err != nil {
+			fmt.Printf("Failed to reverse word '%s': %v\n", word, err)
+		} else {
+			fmt.Printf("Reversed '%s' to '%s'\n", word, response.Message)
+		}
+	}
 }
